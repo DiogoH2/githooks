c318e11ecd09b7eaf75b3798e036c2e3fc100165
diogo <diogo12h91@gmail.com>
2022-07-02 09:03:01 -0300

    useState

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+
+function App() {
+
+  const [repositories, setRepositories] = useState([
+    { id: 1, name: 'repo-teste' },
+    { id: 2, name: 'repo-2' },
+    { id: 3, name: 'repo-3' },
+    { id: 4, name: 'repo-4' },
+  ]);
+
+  function handleAddRepository(){
+    setRepositories([...repositories, {id: Math.random(), name:'novo repo'}])
+  }
+
+
+  return (
+    <>
+      <ul>
+        {repositories.map(repo => <li key={repo.id}>{repo.name}</li>)}
+      </ul>
+      <button onClick={handleAddRepository}>
+        Adicionar Repositorio
+      </button> 
+    </>
+  )
+}
+
+export default App;
